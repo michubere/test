3bc43a8bb2337c1ad7435ca78f9fd38b065fce7d
Michelle <michelle@Michelles-MacBook-Pro.local>
2022-03-08 16:22:32 -0300

    Variables

---
+++ variables.py
@@ -0,0 +1,103 @@
+#%% Celda 1
+
+x = 1
+
+print(x)
+
+x = 3.5
+
+print(x)
+
+name = "Michelle"
+
+print(name)
+
+b = True
+
+print(b)
+
+b = 1
+
+#%% Celda 2
+
+if 0 > 1 and 2 < 3 or 4 >= 5:
+    print("Hello, Class!")
+elif True == True:
+    print("¡Es verdad!")
+else:
+    print("Chau")
+
+#%% Celda 3
+
+value1 = 15
+
+value2 = 0.05
+
+name = 'python script'
+
+datum = False
+
+#%% Celda 4
+x1 = 100
+
+x2 = 20
+
+x3 = -5
+
+y = x1 + x2 + x3
+
+z = x1 - x2 * x3
+
+w = y - z
+
+w1 = (x1 + x2 + x3) - (x1 - x2 * x3)
+
+#%% Celda 5
+
+num = 23
+
+data = True
+
+var = 40.0
+
+res1 = num+data
+
+res2 = data/var
+
+res3 = num*var
+
+#%% Celda 6
+
+result = 70
+
+data = False
+
+value = '158'
+
+var1 = result*data
+
+var2 = data+value
+
+var3 = result/value
+
+#%%Celda 7
+
+result = 5
+
+value = '158'
+
+location = 'payunia'
+
+#%% Celda 8
+
+name = 'Mike'
+
+phrase = 'needs a coffee'
+
+full_phrase = name + phrase
+
+extended_phrase = full_phrase + ' urgente!'
+
+#%% Celda 9
+
+
